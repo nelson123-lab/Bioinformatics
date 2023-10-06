e3628bb0324b16744ee8870be646f9ffd71a202c
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-10-06 16:00:18 -0500

    final

--- 14_Frequency_array_string.py
+++ 14_Frequency_array_string.py
@@ -1,25 +1,26 @@
-# Finding all possible combinations of DNA base with length = k
-def Generate_kmer(k):
-    DNAbases = ['A', 'C', 'G', 'T']
-    for _ in range(k - 1):
-        DNAbases = [(i + j) for i in DNAbases for j in DNAbases]
-    return DNAbases
-
 # Finding the count of each kmer within the DNA_string
-def PatternCount(DNA_string, Pattern):
+def Pattern_count(DNA_string, Pattern):
     # Finding the count each time and returning it since it runs within a for loop.
     Count = sum(1 for i in range(len(DNA_string) - len(Pattern) + 1) if DNA_string[i:i+len(Pattern)] == Pattern)
     return Count
 
-# Finding the frequencey array that holds the number of times that the i-th kmer appears in the string.
-def Frequency_array(DNA_string, k):
-    # Generating the frequency array
-    Freq_array = [PatternCount(DNA_string, pattern) for pattern in Generate_kmer(k)]
-    # Printing the output in the required format as strings.
-    print(" ".join(map(str, Freq_array)))
-
-if __name__ == "__main__":
-    with open("DNA_data.txt", "r") as f1:
-        DNA_string = f1.readline().strip()
-        k = int(f1.readline().strip())
-    Frequency_array(DNA_string, k)
+# Finding all possible combinations of DNA base with length = k
+def Generate_kmer(k):
+    bases = ['A', 'C', 'G', 'T']
+    # Making a copy of the DNA bases.
+    DNA_bases = bases
+    # Generating combinations of DNA base, this can also be done using itertools.product().
+    for _ in range(k - 1):
+        DNA_bases = [i+j for i in DNA_bases for j in bases]
+    return DNA_bases
+   
+with open('DNA_data.txt', 'r') as f1:
+    lines = f1.read().split('\n')
+# Loading the DNA string and k values.
+DNA_string, k  = lines[0], int(lines[1])
+# Generating the kmers.
+kmer = Generate_kmer(k)
+with open("Output_dataset.txt", "w") as file:
+    for pattern in kmer:
+        count = Pattern_count(DNA_string, pattern)
+        file.write(str(count) + " ")
